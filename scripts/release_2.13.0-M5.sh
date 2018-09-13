346f5d2062d6d720f46af1239198246fa526afc9
Kazuhiro Sera <seratch@gmail.com>
2018-09-13 09:22:35 +0900

    3.3.1 release for Scala 2.13.0-M5

---
+++ scripts/release_2.13.0-M5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbt \
+    ++2.13.0-M5! \
+    core/publishSigned \
+    config/publishSigned \
+    interpolation/publishSigned \
+    interpolation-macro/publishSigned \
+    joda-time/publishSigned \
+    library/publishSigned \
+    mapper-generator-core/publishSigned \
+    streams/publishSigned \
+    syntax-support-macro/publishSigned
